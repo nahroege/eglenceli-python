71a8c71793bb981d98216f8b49664b4bb9bbe671
nahroege <egelila@gmail.com>
2019-01-11 22:01:11 +0300

    kaplumbağa projesi yenilendi

--- kaplumbağa.py
+++ kaplumbağa.py
@@ -1,5 +1,6 @@
-
+import sys
 from turtle import Turtle
 t=Turtle()
 t.shape("turtle")
 t.shapesize(20)
+sys.exit(0)
--- yildiz.py
+++ yildiz.py
@@ -3,6 +3,6 @@ turtle.color('purple', 'navy')
 turtle.begin_fill()
 for i in range(9):
     turtle.forward(250)
-    turtle.left(160) 
+    turtle.left(160)
 turtle.end_fill()
 turtle.done()
